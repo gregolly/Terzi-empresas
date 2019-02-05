c5fd9c52e3fef0d9052219d5a2095fac2e552c4a
gregolly <gregollyff@hotmail.com>
2019-02-05 17:39:12 +0000

    Tentando implementar api google maps.

---
+++ footer.php
@@ -0,0 +1,39 @@
+<footer class="rodape">
+  <div class="rodape-itens">
+    <div class="rodape-wrap">
+      <p>(65) 3326-2055</p>
+      <p>08h - 12h</p>
+      <p>13h - 18h</p>
+    </div>
+
+    <div class="rodape-wrap">
+      <p>Rua Antônio Hortolani,
+        157 N centro 
+        Tangará da Serra/MT</p>
+      <p>contato@terziempresas.com.br</p>
+    </div>
+
+    <div class="rodape-wrap">
+      <div class="rodape-form">
+        <form action="">
+          <input type="text">
+          <input type="text">
+          <input type="text">
+          <button>enviar e-mail</button>
+        </form>
+      </div>
+
+      <div class="rodape-wrap">
+          <img src="" alt="">   
+      </div>
+    </div>
+  </div>
+
+  <div class="copy-dev">
+    <p>Todos os direitos reservados à Terzi Empresas</p>
+    <img src="" alt="">
+  </div>
+</footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php bloginfo('name'); ?></title>
+  <link href="https://fonts.googleapis.com/css?family=Alex+Brush|Lato|Roboto:300,400" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css" />
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/reset.css" />
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" />
+  
+  
+  <?php wp_head(); ?>
+</head>
+<body>
+  <header class="header">
+    <ul class="container adress">
+      <li>(65) 3326-2055</li>
+      <li>Rua Antônio Hortolani, 157-N, Cond. Comercial Tangará, Centro - Tangará da serra/MT</li>
+      <li>contato@terziempresas.com.br</li>
+      <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/wp-login.png" alt=""></a></li>
+    <ul>
+      
+  </header>
+
+  <section class="slide">
+    
+      <div class="nav-widgets">
+        <nav class="navigation">
+          <a href="#" class="action -first">as empresas</a>
+          <a href="#" class="action">serviços</a>
+          <a href="#" class="action">convênios</a>
+          <a href="#" class="action">noticias</a>
+          <a href="#" class="action">fale conosco</a>
+        </nav>
+        <ul class="social-media">
+            <a href="#" class="-icons"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="-icons"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="-icons"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="-icons"><i class="fab fa-linkedin"></i></a>
+        </ul>
+        <a href="#" class="search"><i class="fas fa-search"></i></a>
+      </div>
+
+      <div class="logo">
+        <div class="logo-text">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo-slide">
+          <h1><span class="-first-name">terzi</span> <span class="-second-name">empresas</span></h1>
+        </div>
+        <p>agronomia, consultoria, planejamento e responsabilidade</p>
+      </div>
+  
+  </section>
