bb611761a1c28e67fa8e7915f8149ada2c7da65c
gregolly <gregollyff@hotmail.com>
2019-02-18 20:24:31 +0000

    Mudando a tela de login padrão do wp.

---
+++ mina-azul.php
@@ -0,0 +1,14 @@
+<?php get_header('interno'); 
+//Template name: mina azul
+?>
+
+<section class="container mina">
+  <div class="wrap">
+    <img src="<?php the_field(''); ?>" alt="logo da mina azul"/>
+    <p><?php the_field(''); ?></p>
+  </div>
+</section>
+
+
+
+<?php get_footer(); ?>
---
+++ terzi-consultoria.php
@@ -0,0 +1,13 @@
+<?php get_header('interno'); 
+//Template name: consultoria
+?>
+
+<section class="container mina">
+  <div class="wrap">
+    <img src="<?php the_field(''); ?>" alt="logo da mina azul"/>
+    <p><?php the_field(''); ?></p>
+  </div>
+</section>
+
+
+<?php get_footer(); ?>
