5ea313735df5b880220e8cbeb5b705bab9bc6534
gregolly <gregollyff@hotmail.com>
2019-02-25 14:38:01 +0000

    Alterações e fazendo formulario funcionar.

--- enviar-email.php
+++ enviar-email.php
@@ -1,22 +1,48 @@
 ﻿<?php
-  //1 – Definimos Para quem vai ser enviado o email
-  $para = "seu-email@dominio.com.br";
-  //2 - resgatar o nome digitado no formulário e  grava na variavel $nome
-  $nome = $_POST['nome'];
-  // 3 - resgatar o assunto digitado no formulário e  grava na variavel //$assunto
-  $telefone = $_POST['telefone'];
-   //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
-  $mensagem = "<strong>Nome:  </strong>".$nome;
-  $mensagem .= "<br>  <strong>Mensagem: </strong>".$_POST['mensagem'];
- 
-//5 – agora inserimos as codificações corretas e  tudo mais.
-  $headers =  "Content-Type:text/html; charset=UTF-8\n";
-  $headers .= "From:  www.terziempresas.com.br<www.terziempresas.com.br>\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
-  $headers .= "X-Sender:  <www.terziempresas.com.br>\n"; //email do servidor //que enviou
-  $headers .= "X-Mailer: PHP  v".phpversion()."\n";
-  $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-  $headers .= "Return-Path:  <www.terziempresas.com.br>\n"; //caso a msg //seja respondida vai para  este email.
-  $headers .= "MIME-Version: 1.0\n";
- 
-mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
-  ?>
+
+// Trocar os valores abaixo
+
+$emailenvio = 'claudio@terziempresas.com.br';
+$assunto = 'Formulario terzi empresas';
+$url = 'http://www.terziempresas.com.br';
+
+// Mude até aqui apenas
+
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$mensagem = $_POST['mensagem'];
+
+$body = "$mensagem\n\n---------------\n\n$nome\n$telefone";
+
+if ($_POST['leaveblank'] != '' or $_POST['dontchange'] != 'http://') {
+
+  echo "Não foi possível enviar o e-mail. Tente novamente ou entre em contato com " . $emailenvio;
+  echo "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $url . "'>";
+
+} else if (isset($_POST['email'])) {
+
+require ('./PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer;
+$mail->CharSet = 'UTF-8';
+$mail->WordWrap = 70;
+$mail->addAddress($emailenvio);
+
+$mail->From = $email;
+$mail->FromName = $nome;
+$mail->AddReplyTo($email, $nome);
+$mail->Subject = $assunto;
+
+$mail->Body = $body;
+
+if(!$mail->send()) {
+  echo "Não foi possível enviar o e-mail. Tente novamente ou entre em contato com " . $emailenvio;
+  echo "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $url . "'>";
+} else {
+  echo "E-mail enviado com sucesso!";
+  echo "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=" . $url . "'>";
+}
+
+}
+
+?>
--- functions.php
+++ functions.php
@@ -51,8 +51,8 @@ add_action('wp_enqueue_scripts', 'terzi_add_script_rodape');
 
 function terzi_add_script_rodape(){
   wp_enqueue_script('jquery-plugin', get_stylesheet_directory_uri() . '/js/jquery.js');
-  wp_enqueue_script('background_slide', get_stylesheet_directory_uri() . '/js/background_slide.js');
 	wp_enqueue_script('search-form', get_stylesheet_directory_uri() . '/js/search.js');
+	wp_enqueue_script('form-mailer', get_stylesheet_directory_uri() . '/js/form.js');
 	wp_enqueue_script('menu-overlay', get_stylesheet_directory_uri() . '/js/menu-responsive.js');
 	wp_enqueue_script('menu-responsivo-interno', get_stylesheet_directory_uri() . '/js/menu-responsivo-interno.js');
 	wp_enqueue_script('contador', get_stylesheet_directory_uri() . '/js/count.js');
